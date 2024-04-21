4679d56ca5a029f8cdff9f8894b8afac0cb0a7c9
Bennie Kowalski <65255810+Code4Purple@users.noreply.github.com>
2024-04-21 17:58:09 -0500

    page 33 bookmark

---
+++ nnfs_sentdex_book/first_neurons.py
@@ -0,0 +1,26 @@
+# A Single Neuron
+
+# 3 Inputs , 3 Weights, 1 Bias
+inputs = [1,2,3]
+weights = [0.2, 0.8, -0.5]
+bias = 2
+
+output = (inputs[0] * weights[0] + 
+	  inputs[1] * weights[1] + 
+	  inputs[2] * weights[2] + bias)
+
+print(output) # it should be 2.3
+
+# 4 Inputs, 4 Weights, 1 Bias
+inputs2 = [1.0, 2.0, 3.0, 2.5]
+weights2 = [0.2, 0.8, -0.5, 1.0]
+bias2 = 2.0;
+
+output2 = (inputs2[0] * weights2[0] + 
+           inputs2[1] * weights2[1] + 
+	   inputs2[2] * weights2[2] + 
+	   inputs2[3] * weights2[3] + bias2)
+
+print(output2) # it should be 4.8
+
+
---
+++ nnfs_sentdex_book/layered_neurons.py
@@ -0,0 +1,58 @@
+# Layered Neurons
+# 4 Inputs, 3 sets of weights, 3 Bias, 3 Output Value becuase of 3 Neurons
+inputs = [1,2,3,2.5]
+
+weights1 = [0.2, 0.8, -0.5, 1]
+weights2 = [0.5, -0.91, 0.26, -0.5]
+weights3 = [-0.26, -0.27, 0.17, 0.87]
+
+bias1 = 2
+bias2 = 3
+bias3 = 0.5
+
+outputs = [
+	# Neuron 1 :
+	inputs[0] * weights1[0] + 
+	inputs[1] * weights1[1] +
+	inputs[2] * weights1[2] + 
+	inputs[3] * weights1[3] + bias1,
+
+	#Neuron 2 : 
+	inputs[0] * weights2[0] + 
+	inputs[1] * weights2[1] + 
+	inputs[2] * weights2[2] +
+	inputs[3] * weights2[3] + bias2,
+
+	#Neuron 3 : 
+	inputs[0] * weights3[0] + 
+	inputs[1] * weights3[1] + 
+	inputs[2] * weights3[2] + 
+	inputs[3] * weights3[3] + bias3]
+
+print(outputs) # It should be [4.8, 1.21, 2.385]
+
+# Scale-able Loops for dynamically-sized inputs and layers
+
+inputs_v2 = [1,2,3,2.5]
+weights_v2 = [[0.2, 0.8 -0.5, 1],
+	      [0.5, -0.91, 0.26, -0.5],
+	      [-0.26, -0.27, 0.17, 0.87]]
+biases_v2 = [2,3,0.5]
+
+# Output of Current Layer
+layer_outputs = []
+# For each neuron
+for neuron_weights, neuron_bias in zip(weights_v2, biases_v2):
+	# Zeroed output of given neuron
+	neuron_output = 0
+	# For each input and weight to the neuron
+	for n_input, weight in zip(inputs_v2, neuron_weights):
+		# Multiply this input by associated weight
+		# and add to the neuron's output variable
+		neuron_output += n_input * weight
+	# Add bias
+	neuron_output += neuron_bias
+	# Put neuron's result to the layer's output list
+	layer_outputs.append(neuron_output)
+
+print(layer_outputs) # should be [4.8, 1.21, 2.385]
